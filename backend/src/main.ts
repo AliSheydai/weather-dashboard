70fd4ef34e07bb2c5b4d6dbd17d0e404d18ea0f6
AliSheydai <152776609+AliSheydai@users.noreply.github.com>
2026-08-12 18:10:13 +0330

    Added vercel domain for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://weather-dashboard-gavu.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
